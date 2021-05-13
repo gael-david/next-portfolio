5e58e829128b6c474798107ba0239cff6d64aa77
Gaël David <gaeldavid44@gmail.com>
2021-05-13 12:52:12 +0200

    hotfix: add seo files

---
+++ components/SEO/SEO.js
@@ -0,0 +1,37 @@
+const SEO = () => {
+  return (
+    <>
+      <meta name="title" content="Gaël David"></meta>
+      <meta
+        name="description"
+        content="My name is Gaël David. I am a French self-taught Web Developer currently working on the movie lover's app Melies and the environment open data aggregator Ecolometrics. My favorite technologies right now are React, Next.js, Node.js and MongoDB."
+      ></meta>
+      <meta property="og:type" content="website"></meta>
+      <meta property="og:url" content="https://www.gaeldavid.fr/"></meta>
+      <meta property="og:title" content="Gaël David"></meta>
+      <meta
+        property="og:description"
+        content="My name is Gaël David. I am a French self-taught Web Developer currently working on the movie lover's app Melies and the environment open data aggregator Ecolometrics. My favorite technologies right now are React, Next.js, Node.js and MongoDB."
+      ></meta>
+      <meta
+        property="og:image"
+        content="/images/gael-david-site-image.png"
+        alt="Gaël David Website Image"
+      ></meta>
+      <meta property="twitter:card" content="summary_large_image"></meta>
+      <meta property="twitter:url" content="https://www.gaeldavid.fr/"></meta>
+      <meta property="twitter:title" content="Gaël David"></meta>
+      <meta
+        property="twitter:description"
+        content="My name is Gaël David. I am a French self-taught Web Developer currently working on the movie lover's app Melies and the environment open data aggregator Ecolometrics. My favorite technologies right now are React, Next.js, Node.js and MongoDB."
+      ></meta>
+      <meta
+        property="twitter:image"
+        content="/images/gael-david-site-image.png"
+        alt="Gaël David Website Image"
+      ></meta>
+    </>
+  );
+};
+
+export default SEO;
